98eaea44876371f67bf52185197b83d078ed26a1
levanbang376 <levanbang376@gmail.com>
2023-11-11 00:54:34 +0700

    [Bugfix] Fix UI

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import {
   MenuFoldOutlined,
   MenuUnfoldOutlined,
@@ -7,15 +7,31 @@ import {
   VideoCameraOutlined,
 } from "@ant-design/icons";
 import { Layout, Menu, Button, theme } from "antd";
+import { IconContext } from "react-icons";
+import { FaUserCircle } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import UserContext from "../../Context";
+import { Link } from "react-router-dom";
 
 import UserList from "../ManagerUser/ManagerUser";
 
 const { Header, Sider, Content } = Layout;
 const HomePage = () => {
   const [collapsed, setCollapsed] = useState(false);
+  const [showOptions, setShowOptions] = useState(false);
   const {
     token: { colorBgContainer },
   } = theme.useToken();
+
+  const navigate = useNavigate();
+  const { user, setUser } = useContext(UserContext);
+
+  React.useEffect(() => {
+    if (Object.keys(user).length === 0) {
+      navigate("/login");
+    }
+  }, []);
+
   return (
     <Layout>
       <Sider trigger={null} collapsible collapsed={collapsed}>
@@ -50,16 +66,84 @@ const HomePage = () => {
             background: colorBgContainer,
           }}
         >
-          <Button
-            type="text"
-            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-            onClick={() => setCollapsed(!collapsed)}
+          <div
             style={{
-              fontSize: "16px",
-              width: 64,
-              height: 64,
+              display: "flex",
+              justifyContent: "space-between",
             }}
-          />
+          >
+            <Button
+              type="text"
+              icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+              onClick={() => setCollapsed(!collapsed)}
+              style={{
+                fontSize: "16px",
+                width: 64,
+                height: 64,
+              }}
+            />
+            <IconContext.Provider value={{ color: "black", size: "1.5em" }}>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  marginRight: "24px",
+                }}
+              >
+                <div style={{ marginRight: "12px" }}>{user.name}</div>
+                <div
+                  style={{
+                    cursor: "pointer",
+                    display: "flex",
+                    alignItems: "center",
+                    position: "relative",
+                  }}
+                >
+                  <FaUserCircle onClick={() => setShowOptions(!showOptions)} />
+                  {showOptions && (
+                    <div
+                      style={{
+                        width: "80px",
+                        position: "absolute",
+                        right: "0",
+                        boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",
+                        top: "27px",
+                        backgroundColor: "white",
+                        zIndex: "500",
+                      }}
+                    >
+                      <Link
+                        style={{
+                          height: "40px",
+                          display: "flex",
+                          alignItems: "center",
+                          paddingLeft: "12px",
+                          borderBottom: "1px solid #c4c4c4",
+                          color: "black",
+                        }}
+                        to="/profile"
+                      >
+                        Profile
+                      </Link>
+                      <Link
+                        style={{
+                          height: "40px",
+                          display: "flex",
+                          alignItems: "center",
+                          paddingLeft: "12px",
+                          color: "black",
+                        }}
+                        to="/login"
+                        onClick={() => setUser({})}
+                      >
+                        Log out
+                      </Link>
+                    </div>
+                  )}
+                </div>
+              </div>
+            </IconContext.Provider>
+          </div>
         </Header>
         <Content
           style={{
--- src/index.js
+++ src/index.js
@@ -9,6 +9,6 @@ import Router from "./Router";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router />
+    <App />
   </React.StrictMode>
 );
